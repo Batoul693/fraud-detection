fb25bdba662e4d2d1331f982a380a34f2eab6968
Batoul <albatoulali693@gmail.com>
2026-03-02 03:25:07 -0800

    .

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ Internal Risk Monitoring Unit
 
 
 
-app = Flask("Fraud Detection System")
+app = Flask("__name__")
 app.secret_key = 'admin_secret_key_123'
 
 # ===================== LOAD DATA & MODELS =====================
